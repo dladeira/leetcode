5f54e8c451a0628383c12992c0601031805a20e9
dladeira <dladeiratx@gmail.com>
2026-02-16 10:54:15 +0100

    feat: improved 62 unique paths (forgot to commit)

--- 62.unique-paths.ts
+++ 62.unique-paths.ts
@@ -7,23 +7,35 @@
 
 // @lc code=start
 function uniquePaths(m: number, n: number): number {
-	return findPaths(m, n);
-}
+	// We only need one row of memory.
+	// Each cell will represent the number of ways to reach it.
+	const row = new Array(n).fill(1);
 
-let precalculatedMap: Map<string, number> = new Map();
-function findPaths(height: number, width: number): number {
-	if (height == 1 || width == 1) {
-		return 1;
+	for (let i = 1; i < m; i++) {
+		for (let j = 1; j < n; j++) {
+			// The new value is the current value (from the top)
+			// + the value to the left.
+			row[j] = row[j] + row[j - 1];
+		}
 	}
 
-	let key = Math.max(height, width) + "x" + Math.min(height, width);
-
-	if (precalculatedMap.has(key)) return precalculatedMap.get(key)!;
-
-	let value = findPaths(height - 1, width) + findPaths(height, width - 1);
-
-	precalculatedMap.set(key, value);
-	return value;
+	return row[n - 1];
 }
+
+// let precalculatedMap: Map<string, number> = new Map();
+// function findPaths(height: number, width: number): number {
+// 	if (height == 1 || width == 1) {
+// 		return 1;
+// 	}
+
+// 	let key = Math.max(height, width) + "x" + Math.min(height, width);
+
+// 	if (precalculatedMap.has(key)) return precalculatedMap.get(key)!;
+
+// 	let value = findPaths(height - 1, width) + findPaths(height, width - 1);
+
+// 	precalculatedMap.set(key, value);
+// 	return value;
+// }
 
 // @lc code=end
